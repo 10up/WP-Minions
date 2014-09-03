cba02ea5628193aa3350e47e8d63397afa342b8f
Chris Marslender <chrismarslender@gmail.com>
2014-09-03 16:57:22 -0700

    Consolidate to one class for both client and worker. Add multisite support and support for multiple installs on the same server (WP_ASYNC_TASK_SALT)

--- async-task.php
+++ async-task.php
@@ -10,6 +10,14 @@
  *
  */
 
+/*
+ * Users with setups where multiple installs share a common wp-config.php or $table_prefix can use this to segregate
+ * jobs from each site
+ */
+if ( ! defined( 'WP_ASYNC_TASK_SALT' ) ) {
+	define( 'WP_ASYNC_TASK_SALT', '' );
+}
+
 /**
  * Adds a single async task to gearman
  *
@@ -24,62 +32,87 @@ function wp_async_task_add( $hook, $args ) {
 
 function wp_async_task_init() {
 	$GLOBALS['wp_async_task'] = new WP_Async_Task();
-
-	if ( defined( 'DOING_ASYNC' ) && DOING_ASYNC ) {
-		$GLOBALS['wp_async_task_runner'] = new WP_Async_Task_Runner();
-	}
-}
-
-function wp_async_task_switch_to_blog( $blog_id ) {
-	global $wp_async_task;
-
-	return $wp_async_task->switch_to_blog( $blog_id );
 }
 
 class WP_Async_Task {
 
+	/**
+	 * @var GearmanClient
+	 */
+	protected $_client;
+
+	/**
+	 * @var GearmanWorker
+	 */
+	protected $_worker;
+
+
+	public function __construct() {
+		global $gearman_servers;
+
+		$this->_client = new GearmanClient();
+		if ( empty( $gearman_servers ) ) {
+			$this->_client->addServer();
+		} else {
+			$this->_client->addServers( implode( ',', $gearman_servers ) );
+		}
+
+		if ( defined( 'DOING_ASYNC' ) && DOING_ASYNC ) {
+			$this->_worker = new GearmanWorker();
+
+			if ( empty( $gearman_servers ) ) {
+				$this->_worker->addServer();
+			} else {
+				$this->_worker->addServers( implode( ',', $gearman_servers ) );
+			}
+		}
+	}
+
 	//todo may need a CPT to track jobs in the database - For now just storing in Gearman - Will be problematic if gearmand restarts!
 
 	public function add( $hook, $args ) {
-		global $gearman_servers;
-
-		$client = new GearmanClient();
-
-		if ( empty( $gearman_servers ) ) {
-			$client->addServer();
-		} else {
-			$client->addServers( implode( ',', $gearman_servers ) );
-		}
-
 		$jobdata = array();
 		$jobdata['hook'] = $hook;
 		$jobdata['args'] = $args;
+		$jobdata['blog_id'] = ( function_exists( 'is_multisite' ) && is_multisite() ) ? get_current_blog_id() : null;
 
-		return $client->doBackground( 'WP_Async_Task', json_encode( $jobdata ) ); // todo Need blog_id, possibly salt value (for multiple single-sites)
+		return $this->_client->doBackground( $this->gearman_function(), json_encode( $jobdata ) );
 	}
 
-	public function switch_to_blog( $blog_id ) {
-		// todo switch_to_blog
-	}
-}
+	/**
+	 * Returns the gearman function group for this install
+	 *
+	 * @return string
+	 */
+	public function gearman_function() {
+		$key = '';
 
-class WP_Async_Task_Runner {
-
-	public function work() {
-		global $gearman_servers;
-
-		$worker = new GearmanWorker();
-
-		if ( empty( $gearman_servers ) ) {
-			$worker->addServer();
-		} else {
-			$worker->addServers( implode( ',', $gearman_servers ) );
+		if ( WP_ASYNC_TASK_SALT ) {
+			$key .= WP_ASYNC_TASK_SALT . ':';
 		}
 
-		// We are working with the WP_Async_Task function
-		$worker->addFunction( 'WP_Async_Task', array( 'WP_Async_Task_Runner', 'work' ) );
+		$key .= 'WP_Async_Task';
 
-		// WORK!
-		while( $worker->work() );
+		return $key;
+	}
+
+
+	/* Task Runner */
+	public function work() {
+		$this->_worker->addFunction( $this->gearman_function(), array( $this, 'do_job' ) );
+
+		while( $this->_worker->work() );
+	}
+
+	public function do_job( $job ) {
+		$job_data = json_decode( $job->workload() );
+
+		if ( function_exists( 'is_multisite' ) && is_multisite() && $job_data->blog_id ) {
+			switch_to_blog( $job_data->blog_id );
+		}
+
+		do_action( $job_data->hook, $job_data->args );
+
+		return true;
 	}
 }
