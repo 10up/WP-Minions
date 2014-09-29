ffbb1321c080f8e2f4fbfc674d7c5d4163e1e33d
Chris Marslender <chrismarslender@gmail.com>
2014-09-29 15:49:00 -0700

    Error if gearman is not configured correctly for now, until the fallback is implemented correctly

--- inc/class-wp-async-task-fallback.php
+++ inc/class-wp-async-task-fallback.php
@@ -4,6 +4,7 @@ class WP_Async_Task_Fallback extends WP_Async_Task {
 
 	public function add( $hook, $args ) {
 		// todo need to implement this
+		throw new Exception( "Gearman not configured (using callback class) - Fallback not complete yet though :-) ");
 	}
 
 }
