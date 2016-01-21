1aec213d5592074002aa0a8eb402b60e76e70af9
Darshan Sawardekar <darshan@sawardekar.org>
2016-01-21 16:40:37 +0530

    Adds Base Client.

---
+++ includes/WpGears/Client.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WpGears;
+
+/**
+ * Base class for all WpGears Clients. A Client is responsible for
+ * adding jobs to it's Queue. It may optionally perform additional
+ * initialization to setup it's initial state.
+ */
+abstract class Client {
+
+	/**
+	 * Any implementation specific initialization goes here.
+	 *
+	 * @return bool True or false based on whether the client was registered
+	 */
+	abstract function register();
+
+	/**
+	 * Adds the job to the Queue. Clients may receive multiple calls to
+	 * add jobs to the Queue.
+	 *
+	 * @param string $hook The action hook name for the job
+	 * @param array $args Optional arguments for the job
+	 * @param string $priority Optional priority of the job
+	 * @return bool true or false depending on the Client
+	 */
+	abstract function add( $action, $params = array(), $priority );
+
+}
